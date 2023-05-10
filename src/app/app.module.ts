f91a7ba1992907f83bf3525dd909ddfd957fed11
jotsamikael <81319586+jotsamikael@users.noreply.github.com>
2023-05-10 14:37:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingComponent } from './landing/landing.component';
+import { FullstackComponent } from './fullstack/fullstack.component';
+import { WhatsappComponent } from './whatsapp/whatsapp.component';
+import { WordpressComponent } from './wordpress/wordpress.component';
+import { ServicesComponent } from './services/services.component';
+import { VideoComponent } from './video/video.component';
+import { DesignComponent } from './design/design.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactComponent,
+    LandingComponent,
+    FullstackComponent,
+    WhatsappComponent,
+    WordpressComponent,
+    ServicesComponent,
+    VideoComponent,
+    DesignComponent,
+    AboutComponent,
+    BlogComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
